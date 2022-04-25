a48abde89d189d5bc7764e053f96da8fa2490c52
Jesse Fry <jfry7@byui.edu>
2022-04-25 09:43:47 -0600

    first commit

---
+++ html.py
@@ -0,0 +1,23 @@
+<!DOCTYPE HTML>
+<HTML>
+    <Main> #Information UNIQUE for the page
+    <HEADER> #At the top of a page
+    <FOOTER> #At the bottom of a page
+    <Article> #A header and a footer can go into an article
+    <DIV> #Just a box that has all sorts of stuff put into it
+    <HEAD>
+    <TITLE> My Page </TITLE>
+    </HEAD> #Not really shown on the page!
+    <BODY>
+    <P> = Paragraph
+        <Sections> #Can go in Paragraph
+    </P>
+    <h1> - <h6> #Heading 1 through 6
+        #This is seen on the page. Other things are not usually seen! (BODY)
+    </BODY>
+    </HTML>
+    <!-- --> #comments
+    <aside> #ADS/Or anything on the side that doesn't really matter
+attribute: Something = 'Something' 
+    Key = value
+    Example: <html lang = "en-us"
